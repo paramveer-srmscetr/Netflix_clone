db9b174c34a32e0a1bc7eae5473c922b74184ea7
Paramveer singh <paramveerchauhan2000@gmail.com>
2023-08-07 15:31:14 +0530

    netflix
    Co-Authored-By: Paramveer singh <82169480+paramveer-srmscetr@users.noreply.github.com>

---
+++ netflix-clone/netflix-api/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const userRoutes = require("./routes/UserRoutes");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());mongodb://localhost:27017/netfli
+
+mongoose
+  .connect("mongodb+srv://param200:ishu@cluster0.ya341kg.mongodb.net/netflix", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use("/api/user", userRoutes);
+
+app.listen(5000, () => {
+  console.log("server started on port 5000");
+});
